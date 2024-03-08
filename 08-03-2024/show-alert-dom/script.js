6b1c659ad86b1300598551d7adfa6e7eb5bca469
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-09 01:29:22 +0530

    show alart dom

---
+++ 08-03-2024/show-alert-dom/script.js
@@ -0,0 +1,18 @@
+function showFirstName() {
+  // Complete to show an alert with your first name
+  let Name = document.querySelector("#name").value;
+  alert(Name);
+}
+
+function showLastName() {
+  // Complete to show an alert with your last name
+  let lastName = document.querySelector("#lastname").value;
+  alert(lastName);
+
+}
+
+function showFullName() {
+  // Complete to show an alert with your full name (firstname lastname)
+  let fullnam = document.querySelector("#fullname").value;
+  alert(fullnam);
+}
