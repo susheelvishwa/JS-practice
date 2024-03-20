1460ac0a46609b28dd7277a00e4d3bf64d962eaf
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-20 16:45:04 +0530

    dropdowun box

---
+++ 15-03-2024/jankit.js
@@ -0,0 +1,4 @@
+console.log(typeof null);
+
+// When you use typeof with null, it returns "object" instead of "null". This behavior is a mistake in the language design, and it's well-documented. Here's an example:
+
